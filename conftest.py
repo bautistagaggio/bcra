e1303c1341c9f688528c7a58249204f84ad46a6f
Bautista Gaggiotti <bautista.f.gaggiotti@pwc.com>
2021-09-08 15:14:42 -0300

    chromedriver manager

--- conftest.py
+++ conftest.py
@@ -3,21 +3,24 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import logging
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(params=["chrome"], scope="class")
 def init_driver(request):
     if request.param == "chrome":
-        if TestData.Headless == "On":
+        chrome_driver = ChromeDriverManager().install()
+        if TestData.Headless == "On":   
             options = Options()
             options.add_argument('--headless')
             options.add_argument('--disable-gpu')
-            web_driver = webdriver.Chrome(executable_path='chromedriver', options=options)
+            web_driver = webdriver.Chrome(chrome_driver, options=options)
         else:
-            web_driver = webdriver.Chrome(executable_path='chromedriver')
+            web_driver = webdriver.Chrome(chrome_driver)
     request.cls.driver = web_driver
     yield
     web_driver.close()
+
 
 @pytest.fixture(scope="class")
 def info_logging(request):
