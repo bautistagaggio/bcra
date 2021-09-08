05d408d9787fef035d3aa460e33a77182b13a3f8
Bautista <bautistagaggiotti@gmail.com>
2021-09-08 00:32:56 -0300

    algo

--- conftest.py
+++ conftest.py
@@ -1,11 +1,16 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import logging
+
 
 @pytest.fixture(params=["chrome"], scope="class")
 def init_driver(request):
     if request.param == "chrome":
-        web_driver = webdriver.Chrome(executable_path='chromedriver.exe')
+        # options = Options()
+        # options.add_argument('--headless')
+        # options.add_argument('--disable-gpu')
+        web_driver = webdriver.Chrome(executable_path='chromedriver')
     request.cls.driver = web_driver
     yield
     # web_driver.close()
--- test_script.py
+++ test_script.py
@@ -9,37 +9,33 @@ import time
 @pytest.mark.usefixtures('info_logging')
 class Test_Script(BaseTest):
 
-    def test_script(self):
+    def test_generate_excel(self):
+
         self.BCRApage = BCRApage(self.driver)
         self.BCRApage.SendDateDesde()
         self.BCRApage.SendDateHasta()
         self.export_values()
     
 
+
+
     def export_values(self):
+        
         Dat = self.BCRApage.get_table_data()
-        wb = openpyxl.Workbook()
-        ws = wb.active
-        for data in zip(Dat):
-            ws.append(data)
-        wb.save(filename=f"{TestData.Serie}-{TestData.FechaDesde}-{TestData.FechaHasta}.xlsx")
-    
+        if Dat is True:
+            wb = openpyxl.Workbook()
+            ws = wb.active
+            for data in zip(Dat):
+                ws.append(data)
+            wb.save(filename=f"{TestData.Serie}-{TestData.FechaDesde}-{TestData.FechaHasta}.xlsx")
+        else:
+            raise Exception("No se pudo extraer los valores. Ingresar la fecha nuevamente")
+        
     # schedule.every().monday.do(test_date)
     # while True:
     #     schedule.run_pending()
     #     time.sleep(1)
 
-
-# def main():
-#     foo = Test_Data()
-#     foo.test_date()
-
-# if __name__ == "__main__":
-#     main()
-
-
-#pepe
-#pepito
         
     
 
